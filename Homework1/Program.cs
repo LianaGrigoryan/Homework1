608982ba414b1a015a0ae961d897b6f986cd8346
Liana Grigoryan <lgrigoryan25@gmail.com>
2020-08-27 21:22:34 +0400

    Added functions for get minimum and maximum values from array.

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -10,6 +10,82 @@ namespace Homework1
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter array size: ");
+            int arraySize = int.Parse(Console.ReadLine());
+            int[] array = GetArray(arraySize);
+
+            Console.WriteLine();
+            GetPrimeNumbers(array);
+
+            int max = GetMax(array);
+            int min = GetMin(array);
+            Console.WriteLine("Sum of min and max: {0}", min + max);
+        }
+
+        private static int[] GetArray(int arraySize)
+        {
+            int[] array = new int[arraySize];
+            for (int i = 0; i < arraySize; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            return array;
+        }
+
+        private static void GetPrimeNumbers(int[] array)
+        {
+            bool isPrime;
+            for (int i = 0; i < array.Length; i++)
+            {
+                isPrime = true;
+                if (array[i] == 0 || array[i] == 1)
+                {
+                    isPrime = false;
+                }
+                else {
+                    for (int j = 2; j <= Math.Sqrt(array[i]); j++)
+                    {
+                        if (array[i]%j == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                    if (isPrime)
+                    {
+                        Console.WriteLine("Prime number: " + array[i]);
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+
+        private static int GetMin(int[] array)
+        {
+            int min = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+
+            return min;
+        }
+
+        private static int GetMax(int[] array)
+        {
+            int max = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+            }
+
+            return max;
         }
     }
 }
